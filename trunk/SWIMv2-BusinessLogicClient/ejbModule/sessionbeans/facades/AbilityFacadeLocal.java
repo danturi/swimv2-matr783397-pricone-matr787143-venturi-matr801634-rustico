b6f32d05d2ef10c209d6287f0418821464e52416
marco.pricone <marco.pricone@8659c87d-7b90-9cdb-48ff-1ed3e7a40477>
2012-12-31 14:51:44 +0000

    prove varie login

---
+++ trunk/SWIMv2-BusinessLogicClient/ejbModule/sessionbeans/facades/AbilityFacadeLocal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sessionbeans.facades;
+
+import entities.Ability;
+import java.util.List;
+import javax.ejb.Local;
+
+
+/**
+ *
+ * @author MARCO
+ */
+@Local
+
+public interface AbilityFacadeLocal {
+
+    void create(Ability ability);
+
+    void edit(Ability ability);
+
+    void remove(Ability ability);
+
+    Ability find(Object id);
+
+    List<Ability> findAll();
+
+    List<Ability> findRange(int[] range);
+
+    int count();
+    
+}
